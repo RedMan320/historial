3b16fe817d56591af07dc989a39083fd8f43f014
Lucas12github <Lucasmouhsen@gmail.com>
2023-06-05 17:05:18 -0300

    Se modificaron y agregaron validaciones en el front

--- public/js/Validations.js
+++ public/js/Validations.js
@@ -3,114 +3,105 @@ let firstname = document.getElementById("firstname")
 let lastname = document.getElementById("lastname")
 let lastAppointment = document.getElementById("lastAppointment")
 let box = document.getElementById("box")
-let span = document.querySelectorAll(".errores")
-let boxErrores = document.querySelectorAll(".box-errores-front")
-let form = document.getElementById("form")
-let alert = document.getElementById("alert")
-let alertp = document.getElementById("alert-p")
-/* Modal */
-let modal = document.getElementById("myModal");
-let btnModal = document.getElementById("myBtn");
-let spanModal = document.getElementsByClassName("close")[0];
-let btnModalSubmit = document.getElementById("btnModalSubmit")
-function validateField(field, minLength, maxLength, regex) {
-  const value = field.value.trim();
-  const isValid = value.length >= minLength && value.length <= maxLength && regex.test(value);
-  field.style.border = isValid ? "1px solid #10dc60" : "1px solid #f04141";
-  isValid ? boxErrores[field.dataset.index].removeAttribute('style') : null
-}
-function validatesubmit() {
-  const value = field.value.trim();
-  const isValid = value.length >= minLength && value.length <= maxLength && regex.test(value);
+let span = document.getElementById("errores-front")
+let boxErrores = document.getElementById("box-errores-front")
+let form = document.getElementById('form');
 
-}
-window.addEventListener("load", () => {
-  hc.addEventListener("input", () => {
-    /* validateField(hc, 4, 20, /^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9]+$/); */
-    const value = hc.value.trim();
-    const isValid = value.length >= 4 && value.length <= 20 && /^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9]+$/.test(value) && /\d/.test(value);
-    hc.style.border = isValid ? "1px solid #10dc60" : "1px solid #f04141";
-    isValid ? boxErrores[hc.dataset.index].removeAttribute('style') : null
-    /* if (value.length >= minLength && value.length <= maxLength && regex.test(value) && /\d/.test(hc.value)) {
-      field.style.border = isValid ? "1px solid #10dc60" : "1px solid #f04141";
-      isValid ? boxErrores[field.dataset.index].removeAttribute('style') : null
-    } */
-  });
+const validationHC = [
+  { validator: value => value.toLowerCase() !== '', message: 'La historia clínica es obligatoria', index: 0 },
+  { validator: value => value.length >= 4 && value.length <= 20, message: 'La historia clínica debe tener entre 4 y 20 caracteres', index: 0 },
+  { validator: value => /^(?=.*[0-9])/.test(value), message: 'La historia clínica debe contener letras y números', index: 0 }
+]
+const validationFirstName = [
+  { validator: value => value.toLowerCase() !== '', message: 'El nombre no debe estar vacío', index: 1 },
+  { validator: value => value.length >= 2 && value.length <= 100, message: 'El nombre debe tener entre 2 y 100 caracteres', index: 1 },
+  { validator: value => /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(value), message: 'El nombre solo debe contener letras', index: 1 }
+]
+const validationLastName = [
+  { validator: value => value.toLowerCase() !== '', message: 'El apellido no debe estar vacío', index: 2 },
+  { validator: value => value.length >= 2 && value.length <= 100, message: 'El apellido debe tener entre 2 y 100 caracteres', index: 2 },
+  { validator: value => /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(value), message: 'El apellido solo debe contener letras', index: 2 }
+]
+const validationLastAppointment = [
+  { validator: value => value !== '', message: 'La fecha es obligatoria', index: 3 },
+  { validator: value => !isNaN(Date.parse(value)), message: 'La fecha debe ser válida', index: 3 }
+]
+const validationsBox = [
+  { validator: value => value.toLowerCase() !== '', message: 'El número de caja es obligatorio', index: 4 },
+  { validator: value => value.length >= 1 && value.length <= 20, message: 'La caja debe tener entre 1 y 20 caracteres', index: 4 },
+  { validator: value => /^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9\s]+$/.test(value), message: 'El número de caja solo debe contener letras y números', index: 4 }
+]
 
-  firstname.addEventListener("input", () => {
-    validateField(firstname, 2, 100, /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/);
-  });
 
-  lastname.addEventListener("input", () => {
-    validateField(lastname, 2, 100, /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/);
-  });
+const validateInput = (element, validations) => {
+  let value = element.value.trim();
 
-  lastAppointment.addEventListener("input", () => {
-    const date = new Date(lastAppointment.value).toString() !== "Invalid Date"
-    if (!date || /^[0-9\s]+$/.test(lastAppointment.value.trim())) {
-      lastAppointment.style.border = "1px solid #f04141";
-    } else {
-      lastAppointment.style.border = "1px solid #10dc60";
-      boxErrores[lastAppointment.dataset.index].removeAttribute('style')
+  for (let i = 0; i < validations.length; i++) {
+    const validation = validations[i];
+    if (!validation.validator(value)) {
+      element.style.border = "1px solid #f04141";
+      return validation.message;
     }
-  });
+  }
 
-  box.addEventListener("input", () => {
-    validateField(box, 1, 50, /^[a-zA-ZáéíóúÁÉÍÓÚñÑ0-9]+$/);
-  });
-
-  form.addEventListener("submit", e => {
-    e.preventDefault()
-
-    const fields = [
-      { element: hc, index: 0, errorMessage: "La historia clínica es obligatoria" },
-      { element: firstname, index: 1, errorMessage: "El nombre es obligatorio" },
-      { element: lastname, index: 2, errorMessage: "El apellido es obligatorio" },
-      { element: lastAppointment, index: 3, errorMessage: "La fecha es obligatoria" },
-      { element: box, index: 4, errorMessage: "La caja es obligatoria" }
-    ];
-    let erroresActivos = [];
-    
-    fields.forEach(field => {
-      const { element, index, errorMessage } = field;
-      const value = element.value.trim();
+  element.style.border = "1px solid #10dc60";
+  return '';
+};
 
 
-      if (value.length <= 0) {
-        boxErrores[index].style.display = "flex";
-        span[index].innerHTML = errorMessage;
-        element.style.border = "1px solid #f04141";
-        erroresActivos.push(errorMessage);
-      } else {
-        span[index].innerHTML = "";
-        boxErrores[index].removeAttribute('style');
-      }
-    });
-
-    if (erroresActivos.length === 0) {
-      btnModal.onclick = function () {
-        modal.style.display = "block";
-      }
-      spanModal.onclick = function () {
-        modal.style.display = "none";
-      }
-      window.onclick = function (event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
-      btnModalSubmit.onclick = (e) =>{
-        form.submit();
-      }
-    }
-  })
-  // When the user clicks on the button, open the modal
-  
-
-  // When the user clicks on <span> (x), close the modal
-  
-
-  // When the user clicks anywhere outside of the modal, close it
-  
+hc.addEventListener('input', () => {
+  validateInput(hc, validationHC)
 })
+firstname.addEventListener('input', () => {
+  validateInput(firstname, validationFirstName)
+})
+lastname.addEventListener('input', () => {
+  validateInput(lastname, validationLastName)
+})
+lastAppointment.addEventListener('input', () => {
+  validateInput(lastAppointment, validationLastAppointment)
+})
+box.addEventListener('input', () => {
+  validateInput(box, validationsBox)
+})
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let erroresForm = {};
+
+  const validateForm = (element, validations, index) => {
+    let value = element.value.trim();
+
+    for (let i = 0; i < validations.length; i++) {
+      const validation = validations[i];
+      if (!validation.validator(value)) {
+        element.style.border = "1px solid #f04141";
+        erroresForm[index] = validation.message;
+        boxErrores.style.display = "block";
+        return;
+      }
+    }
+
+    element.style.border = "1px solid #10dc60";
+    delete erroresForm[index];
+  };
+
+  validateForm(hc, validationHC, 0);
+  validateForm(firstname, validationFirstName, 1);
+  validateForm(lastname, validationLastName, 2);
+  validateForm(lastAppointment, validationLastAppointment, 3);
+  validateForm(box, validationsBox, 4);
+
+  const errores = Object.values(erroresForm);
+  if (errores.length > 0) {
+    span.innerHTML = errores.join('.<br>');
+  } else {
+    span.innerHTML = '';
+    const confirmacion = window.confirm("¿Estás seguro de que deseas enviar el formulario?");
+    if (confirmacion) {
+      form.submit();
+      alert("El registro se envió correctamente");
+    }
+  }
+});
 
