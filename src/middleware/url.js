a0057d6089522a312470f394455ec3c0ba1a1c95
lucasmouhsen <lucasmouhsen@gmail.com>
2023-10-24 09:14:50 -0300

    Se cancela ejecucion

--- src/middleware/url.js
+++ src/middleware/url.js
@@ -3,13 +3,6 @@ const path = require('path');
 const datosPath = path.join(__dirname, '../Historial/datos.txt');
 
 module.exports = (req, res, next) => {
-  const url = req.url;
-  const metodo = req.method;
-  const usuarioWindows = process.env.USERNAME;
-  const fechaHora = new Date().toLocaleString();
-
-  const data = `${url}|${metodo}|${usuarioWindows}|${fechaHora}\n`;
-
-  fs.appendFileSync(datosPath, data);
-  next();
-};
+  /* console.log(req.connection.remoteAddress); */
+  next()
+}
